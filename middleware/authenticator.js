5c1d6a98b16a135559dcbfe4526fa32cb0daea68
Mateus_Munis <mateusfreitas2211@gmail.com>
2026-03-09 02:15:04 -0300

    correcao de bugs

--- middleware/authenticator.js
+++ middleware/authenticator.js
@@ -2,10 +2,10 @@ import jwt from 'jsonwebtoken';
 
 const authenticator = (req, res, next) => {
     const token = req.headers['authorization']; 
-    
-    if(!token)return res.status(404).json({message: "Token not found"});
+    const token_split = token.split(' ')[1];
 
-    jwt.verify(token, process.env.SECRET_KEY, (err, user)=>{
+    if(!token_split)return res.status(404).json({message: "Token not found"});
+    jwt.verify(token_split, process.env.SECRET_KEY, (err, user)=>{
         if(err)return res.status(401).json({message: "Token unathorized"});
         req.user = user;
         next();
